f408043f3dc33f3904c19752e12eb13547742334
ParthBadkul <badkul190@gmail.com>
2023-03-27 11:12:42 +0530

    coursel impovement

--- lib/Destination_Screen.dart
+++ lib/Destination_Screen.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hey/models/activity_model.dart';
 import "models/destination_model.dart";
 
 class DestinationScreen extends StatefulWidget {
@@ -19,7 +22,11 @@ class _DestinationScreenState extends State<DestinationScreen> {
         child: Column(
           children: [
             Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+              ),
               child: Stack(
+                fit: StackFit.loose,
                 children: [
                   Container(
                     width: MediaQuery.of(context).size.width,
@@ -30,7 +37,7 @@ class _DestinationScreenState extends State<DestinationScreen> {
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black,
-                          offset: Offset(0, 3),
+                          offset: Offset(3, 3),
                           blurRadius: 20,
                         )
                       ],
@@ -120,14 +127,167 @@ class _DestinationScreenState extends State<DestinationScreen> {
                           width: 1,
                         ),
                         IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              FontAwesomeIcons.sortAmountDownAlt,
-                            ))
+                          onPressed: () {},
+                          icon: Icon(
+                            FontAwesomeIcons.sortAmountDownAlt,
+                          ),
+                        )
                       ],
                     ),
                   )
                 ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.destination.activities.length,
+                itemBuilder: (context, index) {
+                  Activity activity = widget.destination.activities[index];
+                  return Stack(
+                    children: [
+                      Container(
+                        // color: Colors.red,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20),
+                          ),
+                        ),
+                        height: 200,
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.only(left: 5, right: 5),
+                        margin: EdgeInsets.all(5),
+                        child: Card(
+                          elevation: 24,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20),
+                          )),
+                          // color: Colors.amber,
+                          child: Row(
+                            children: [
+                              Container(
+                                // color: Colors.red,
+                                // decoration: BoxDecoration(boxShadow: [
+                                //   BoxShadow(
+                                //     blurRadius: 1,
+                                //     offset: Offset(-13, 0),
+                                //     color: Colors.black26,
+                                //   )
+                                // ]),
+                                width: 170,
+                                height: 200,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                        bottomLeft: Radius.circular(20),
+                                        topRight: Radius.elliptical(6, 7),
+                                        bottomRight: Radius.elliptical(7, 6)),
+                                  ),
+                                  elevation: 10,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                        bottomLeft: Radius.circular(20),
+                                        topRight: Radius.elliptical(6, 7),
+                                        bottomRight: Radius.elliptical(7, 6)),
+                                    child: Image.asset(
+                                      activity.imageUrl,
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.only(top: 10),
+                                        padding: EdgeInsets.only(
+                                          left: 10,
+                                          top: 10,
+                                        ),
+                                        width: 100,
+                                        child: Text(
+                                          activity.name,
+                                          style: TextStyle(
+                                            color: Colors.black87,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      Column(
+                                        children: [
+                                          Container(
+                                            width: 70,
+                                            padding: EdgeInsets.only(
+                                              top: 11,
+                                            ),
+                                            child: Text(
+                                              "₹ ${activity.price * 80}",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 18),
+                                            ),
+                                          ),
+                                          Container(
+                                            margin: EdgeInsets.only(top: 2),
+                                            child: Text(
+                                              'per pax',
+                                              style: TextStyle(
+                                                color: Colors.black38,
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      top: 2,
+                                      left: 4,
+                                    ),
+                                    padding: EdgeInsets.all(5),
+                                    child: Text(
+                                      activity.type,
+                                      style: TextStyle(
+                                        color: Colors.black38,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      top: 2,
+                                      left: 4,
+                                    ),
+                                    padding: EdgeInsets.all(5),
+                                    child: Text('Raiting'),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                },
               ),
             ),
           ],
